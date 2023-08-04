9233ad1ea6d1db7c698cf31da761449fac8245c7
aminul-islam-niloy <niloyftaminul@gmail.com>
2023-08-04 14:32:58 +0600

    Object initialyzer

--- Advance C#/Program.cs
+++ Advance C#/Program.cs
@@ -29,7 +29,7 @@ namespace Advance_C_
         {
 
 //Object initializer in c# 
-            Student student = new Student()
+            Studentinit student = new Studentinit()
             {
                 Id = 1,
                 Name = "Amin",
@@ -41,15 +41,15 @@ namespace Advance_C_
             //Console.WriteLine(name);
 
 //collection inintializer in c#
-            Student student1 = new Student() { Id = 1, Name = "Amin", 
+            Studentinit student1 = new Studentinit() { Id = 1, Name = "Amin", 
                                                        Address = "Dhaka", Age = 10 };
-            Student student2 = new Student() { Id = 2, Name = "Akij", Address = "Dhaka", Age = 20 };
-            Student student3 = new Student() { Id = 3, Name = "Jamil", Address = "Dhaka", Age = 40 };
-            Student student4 = new Student() { Id = 4, Name = "Asgar", Address = "Dhaka", Age = 60 };
+            Studentinit student2 = new Studentinit() { Id = 2, Name = "Akij", Address = "Dhaka", Age = 20 };
+            Studentinit student3 = new Studentinit() { Id = 3, Name = "Jamil", Address = "Dhaka", Age = 40 };
+            Studentinit student4 = new Studentinit() { Id = 4, Name = "Asgar", Address = "Dhaka", Age = 60 };
 
 
 // add student list without add method.
-            List<Student> studentsList1 = new List<Student>()
+            List<Studentinit> studentsList1 = new List<Studentinit>()
             {   student1,
                 student2,
                 student3,
@@ -65,11 +65,11 @@ namespace Advance_C_
 
   // there are another way to initilaze  collection.
             // there in collection section we can init object and add to the list
-            List<Student> students = new List<Student>()
+            List<Studentinit> students = new List<Studentinit>()
             {
-                new Student() {Id=33, Age=10},
-                new Student() {Id=43, Age=20},
-                new Student() {Id=53, Age=30}
+                new Studentinit() {Id=33, Age=10},
+                new Studentinit() {Id=43, Age=20},
+                new Studentinit() {Id=53, Age=30}
             };
 
             // it's regular system to add value on the list
@@ -415,7 +415,7 @@ namespace Advance_C_
 
  // Reflection 
 
-           var  StudentClassInfo  = typeof(Student);
+           var  StudentClassInfo  = typeof(Studentinit);
 
             string assemblyName = StudentClassInfo.Assembly.FullName;
            
