836df78c88e47c2ca7af38bff2502f3b272465ea
aminul-islam-niloy <niloyftaminul@gmail.com>
2023-08-05 17:43:39 +0600

    Inheritance base class is created with deposite and withdraw method and different property

---
+++ Advance C#/OOP/Inheritance/BankAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advance_C_.OOP.Inheritance
+{
+    public  class BankAccount
+    {
+        public string  AccountNo { get; set; }
+        public string CustomerName { get; set;}
+        public double Balance { get; private set; } 
+
+        public double ServiceCharge { get; set; }   
+        public  double  Interestamount { get; set; }
+
+        public string Deposit(double amount)
+        {
+            Balance += amount;
+            return "Deposited";
+
+        }
+
+        public string Withdraw(double amount)
+        {
+            Balance -= amount;
+            return "Withdrawn";
+
+        }
+
+
+
+    }
+}
